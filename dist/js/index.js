14ad1cdec4455d5009231c524f66330ebf49aab3
miloupark <136055426+miloupark@users.noreply.github.com>
2024-11-20 00:49:28 +0900

    first upload

---
+++ dist/js/index.js
@@ -0,0 +1,20 @@
+const randomButton = document.querySelector(".lotto__button");
+const viewNumber = document.querySelector(".lotto__view");
+const minNumber = 1;
+const maxNumber = 45;
+const targetNumber = []; // 1이상 45이하의 중복되지 않는 6개의 숫자!
+
+randomButton.addEventListener("click", () => {
+  targetNumber.length = 0;
+
+  // 중복되지 않는 정수 6개 생성
+  while (targetNumber.length < 6) {
+    const getRandomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+    if (!targetNumber.includes(getRandomNumber)) {
+      targetNumber.push(getRandomNumber);
+    }
+  }
+
+  viewNumber.textContent = targetNumber.join(", ");
+});
