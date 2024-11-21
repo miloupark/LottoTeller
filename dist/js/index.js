42d61b9ffbafb8f666fcd8087915570ed368b571
miloupark <136055426+miloupark@users.noreply.github.com>
2024-11-21 21:04:26 +0900

    feat: 버튼 재클릭 시 생성된 번호 초기화

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,15 @@
 const randomButton = document.querySelector(".lotto__button");
 const viewNumber = document.querySelectorAll(".lotto-number");
-const minNumber = 1;
-const maxNumber = 45;
+const minNumber = 1; // 로또 최소 숫자
+const maxNumber = 45; // 로또 최대 숫자
 const targetNumber = []; // 1이상 45이하의 중복되지 않는 6개의 숫자!
 
 randomButton.addEventListener("click", () => {
+  // 버튼 재클릭 시 생성된 번호 초기화
+  viewNumber.forEach((element) => {
+    element.textContent = "";
+  });
+
   targetNumber.length = 0;
 
   // 중복되지 않는 정수 6개 생성
@@ -16,6 +21,7 @@ randomButton.addEventListener("click", () => {
     }
   }
 
+  // 6개의 숫자 생성 시간 차
   viewNumber.forEach((element, index) => {
     setTimeout(() => {
       element.textContent = targetNumber[index];
